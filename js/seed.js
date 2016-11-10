0c2bdddfdc579c98855d6fca1d8810c8a3a9108f
RyanLafferty <rlafferty1@hotmail.com>
2016-11-10 15:02:51 -0500

    added some js magic

--- js/seed.js
+++ js/seed.js
@@ -1,3 +1,6 @@
+$("#bar").hide();
+$("#line").hide();
+
 function ChangeDataType(selected)
 {
     alert(selected.value);
@@ -5,5 +8,19 @@ function ChangeDataType(selected)
 
 function ChangeDisplayType(selected)
 {
-    alert(selected.value);
+    $("#table").hide();
+    $("#bar").hide();
+    $("#line").hide();
+    if(selected.value == 'Table')
+    {
+        $("#table").show();
+    }
+    else if(selected.value == 'Bar Graph')
+    {
+        $("#bar").show();
+    }
+    else if(selected.value == 'Line Graph')
+    {
+        $("#line").show();
+    }
 }
